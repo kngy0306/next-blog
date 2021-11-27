7da03d4a4a10f83ba434486d74c65312b80e673c
Kona <kitami.kitano20@gmail.com>
2021-11-27 22:34:10 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+## Tutorial Site
+https://nextjs.org/learn/basics/create-nextjs-app?utm_source=next-site&utm_medium=homepage-cta&utm_campaign=next-website
+
+## 日本語翻訳プロジェクト
+https://nextjs-ja-translation-docs.vercel.app/docs/getting-started
+
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -10,8 +12,13 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
+
+        <Image src="/images/profile.jpg" alt="kona" height="400px" width="400px" />
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
