c6271f8181615f486b4f68b32e809572db1d3388
kona <kitami.kitano20@gmail.com>
2022-02-14 23:19:10 +0900

    :white_check_mark: TypeScriptへ以降

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
 import styles from './layout.module.css'
 
-export default function Layout({children}) {
+export default function Layout({children}: {children: JSX.Element}) {
   return <div className={styles.container}>{children}</div>
 }
